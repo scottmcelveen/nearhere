19d5173e66de211e97af9580911f07648de344d5
scottmcelveen <scott.mcelveen@hotmail.com>
2018-02-06 21:43:41 +0000

    :sunrise: initial commit default xamarin forms proj

---
+++ nearhere/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace nearhere
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
