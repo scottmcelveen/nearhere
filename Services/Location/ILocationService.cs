b5c95e963d9a3be2cb32965243e5f63e593cf21a
scottmcelveen <scott.mcelveen@hotmail.com>
2018-03-20 21:19:35 +0000

    Add services project with geolocator plugin

---
+++ Services/Location/ILocationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Services.Location
+{
+    public interface ILocationService
+    {
+        Task<bool> IsLocationAvailableAsync();
+    }
+}
---
+++ Services/Location/LocationService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Plugin.Geolocator;
+
+namespace Services.Location
+{
+    public class LocationService : ILocationService
+    {
+        public async Task<bool> IsLocationAvailableAsync()
+        {
+            return await CrossGeolocator.Current.GetIsGeolocationAvailableAsync();
+        }
+    }
+}
